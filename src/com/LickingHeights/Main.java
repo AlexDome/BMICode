6ecb7e5ad28366b5f94089419cf07901856a94bf
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-02-14 14:18:59 -0500

    Worked on BMI

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,13 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard;
-        Scanner Hfeet;
-        String Hinches;
-        int Hfeet, Hinches;
+        keyboard = new Scanner(System.in);
+        int Hfeet, Hinches, Weight, BMI;
 
         System.out.println("Give me your height with only the feet please.");
-        Hfeet = keyboard.nextLine();
+        Hfeet = keyboard.nextInt();
         System.out.println("Now give me your height with only the inches.");
-        
+        Hinches = keyboard.nextInt();
+        System.out.println("Your feet to meters is " + Hfeet*0.3048+"m.");
+        Hmeters = Hfeet*0.3048;
+        System.out.println("Your inches to centimeters is " + Hinches*2.54+"cm.");
+        System.out.println("About how much do you weigh in pounds (use 5's)");
+        Weight = keyboard.nextInt();
+        System.out.println("Your weight in kilograms is " +Weight*0.453592+"kg.");
+        BMI = Weight/Hmeters;
+        System.out.println("Your BMI is " + BMI);
+
+
+
+
     }
 }
