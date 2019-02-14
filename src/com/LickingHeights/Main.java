1555b532775fb47a39d8d8af48dbf66b2307b039
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-02-14 13:49:40 -0500

    Set Up Scanners and Ints

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,18 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard;
+        Scanner Hfeet;
+        String Hinches;
+        int Hfeet, Hinches;
+
+        System.out.println("Give me your height with only the feet please.");
+        Hfeet = keyboard.nextLine();
+        System.out.println("Now give me your height with only the inches.");
+        
     }
 }
