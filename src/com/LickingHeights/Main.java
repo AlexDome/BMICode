f4dee8160750cc52efaaa0d92e95935d57553dbb
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-02-19 13:47:22 -0500

    Finished all Code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-        int Hfeet, Hinches, Weight, BMI;
+        int Hfeet, Hinches, Weight;
+        double Hmeters, BMI;
 
-        System.out.println("Give me your height with only the feet please.");
+        System.out.println("Welcome to the BMI Calculator, can you");
+        System.out.println("give me your height with only the feet please.");
         Hfeet = keyboard.nextInt();
         System.out.println("Now give me your height with only the inches.");
         Hinches = keyboard.nextInt();
         System.out.println("Your feet to meters is " + Hfeet*0.3048+"m.");
         Hmeters = Hfeet*0.3048;
         System.out.println("Your inches to centimeters is " + Hinches*2.54+"cm.");
-        System.out.println("About how much do you weigh in pounds (use 5's)");
+        System.out.println("About how much do you weigh in pounds?");
         Weight = keyboard.nextInt();
         System.out.println("Your weight in kilograms is " +Weight*0.453592+"kg.");
         BMI = Weight/Hmeters;
         System.out.println("Your BMI is " + BMI);
-
-
-
-
+        System.out.println("Thank you for using my Body Mass Index Calculator.");
     }
 }
